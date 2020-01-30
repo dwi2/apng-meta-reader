8921b8436a0cf37dc5320fa76961d89999d40e7a
dwi2 <dwi2@dwi2.com>
2020-01-31 00:17:05 +0900

    feat: display all from apng-js

---
+++ index.js
@@ -0,0 +1,40 @@
+import parseAPNG from "apng-js";
+import stringifyObject from "stringify-object";
+
+const isPNG = item => {
+  return item && item.kind === "file" && item.type === "image/png";
+};
+
+const erase = () => {
+  document.querySelector("#meta-output").textContent = "Not a PNG";
+};
+
+const handler = evt => {
+  evt.preventDefault();
+  if (evt.type === "drop") {
+    console.log(evt.dataTransfer.items);
+    if (
+      evt.dataTransfer.items &&
+      evt.dataTransfer.items[0] &&
+      isPNG(evt.dataTransfer.items[0])
+    ) {
+      const file = evt.dataTransfer.items[0].getAsFile();
+      file.arrayBuffer().then(buffer => {
+        const apng = parseAPNG(buffer);
+        const output = stringifyObject(apng);
+        console.log(output);
+        document.querySelector("#meta-output").textContent = output;
+      });
+    } else {
+      erase();
+    }
+  } else {
+    erase();
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  ["dragover", "drop"].forEach(evtType => {
+    document.addEventListener(evtType, handler);
+  });
+});
