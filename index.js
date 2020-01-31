947d32f40952a3d279252d295005f2c2de0feeeb
dwi2 <dwi2@dwi2.com>
2020-01-31 12:51:55 +0900

    chore: make output prettier

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
 import parseAPNG from "apng-js";
-import stringifyObject from "stringify-object";
+import filesize from "filesize";
 
-const defaultText = "Drag and drop APNG file here";
+const defaultText = "Please drag and drop APNG file here";
 const notPNGText = "Not a PNG";
 
 const isPNG = item => {
   return item && item.kind === "file" && item.type === "image/png";
 };
 
-const erase = () => {
-  document.querySelector(
-    "#meta-output"
-  ).textContent = `${notPNGText}, ${defaultText}`;
+const metaOutput = document.querySelector('#meta-output');
+const erase = reason => {
+  metaOutput.textContent = `${reason}, ${defaultText}`;
+};
+
+const parseFile = file => {
+  if (!file || !(file instanceof File)) {
+    return;
+  }
+  let meta = {
+    filename: file.name,
+    filesize: file.size
+  };
+
+  file.arrayBuffer().then(buffer => {
+    const apng = parseAPNG(buffer);
+    if (apng instanceof Error) {
+      erase("Not an APNG")
+      return;
+    }
+
+    Object.assign(meta, apng);
+    metaOutput.textContent = `
+      file name: ${meta.filename}
+      file size: ${filesize(meta.filesize)}
+      duration: ${meta.playTime / 1000} s
+      loop: ${meta.numPlays === 0 ? 'infinite' : meta.numPlays}
+      number of frames : ${meta.frames.length}
+      width: ${meta.width} px
+      height: ${meta.height} px`;
+  });
+
 };
 
 const handler = evt => {
   evt.preventDefault();
-  if (evt.type === "drop") {
-    console.log(evt.dataTransfer.items);
-    if (
-      evt.dataTransfer.items &&
-      evt.dataTransfer.items[0] &&
-      isPNG(evt.dataTransfer.items[0])
-    ) {
-      const file = evt.dataTransfer.items[0].getAsFile();
-      file.arrayBuffer().then(buffer => {
-        const apng = parseAPNG(buffer);
-        const output = stringifyObject(apng);
-        console.log(output);
-        document.querySelector("#meta-output").textContent = output;
-      });
-    } else {
-      erase();
-    }
+  if (
+    evt.dataTransfer.items &&
+    evt.dataTransfer.items[0] &&
+    isPNG(evt.dataTransfer.items[0])
+  ) {
+    const file = evt.dataTransfer.items[0].getAsFile();
+    parseFile(file);
+  } else {
+    erase(notPNGText);
   }
 };
 
